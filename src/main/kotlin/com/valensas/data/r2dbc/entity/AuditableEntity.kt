7e4a6b534e221c5d4406d360d45597e7d5144702
Taylan Çapraz <taylan@valensas.com>
2023-01-10 15:01:34 +0300

    make created by and created date fields updateable

--- src/main/kotlin/com/valensas/data/r2dbc/entity/AuditableEntity.kt
+++ src/main/kotlin/com/valensas/data/r2dbc/entity/AuditableEntity.kt
@@ -5,14 +5,17 @@ import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.LastModifiedBy
 import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.relational.core.mapping.InsertOnlyProperty
 import java.time.Instant
 
 open class AuditableEntity<T>(
     @Id
     open val id: T? = null,
     @CreatedDate
+    @InsertOnlyProperty
     open val createdDate: Instant? = null,
     @CreatedBy
+    @InsertOnlyProperty
     open val createdBy: String? = null,
     @LastModifiedDate
     open val updatedDate: Instant? = null,
